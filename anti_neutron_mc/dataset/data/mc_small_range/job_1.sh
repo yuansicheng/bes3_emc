b94a3b3aaf4ec1437771d6ebac037fe943aacab5
Yuan Sicheng <yuansc@lxslc715.ihep.ac.cn>
2022-12-05 10:13:52 +0800

    first commit

--- anti_neutron_mc/dataset/data/mc_small_range/job_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ~/.bashrc
-pwd
-setup_boss
-source /junofs/users/yuansc/bes3_emc/anti_neutron_mc/test/nnpred_env.sh
-i=1
-python ../../codes/sim.py -i $i -e 10000
-python ../../codes/sim2rec.py -i $i
-python ../../codes/rec2root.py -i $i
